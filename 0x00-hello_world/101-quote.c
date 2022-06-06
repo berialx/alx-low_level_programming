5b46be17b40e1057979f80702931309f80a798cf
bereket <emailbereket@gmail.com>
2022-06-06 13:25:44 -0700

    las last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return: 1 if success
  */
 
 int main(void)
 
 {
-	std::cerr << "and that piece of art is useful" - Dora Korpar, 2015-10-19" << std::endl;
-	return 0;
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
