75022b4e3d18bea6c6cbfc486801365d7e9bcd2c
bereket <emailbereket@gmail.com>
2022-06-07 13:10:06 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Prints lowercase alphabets
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+
+	return (0);
+}
