3cbc8e933e9590c40b61e657455da4b4857be081
bereket <emailbereket@gmail.com>
2022-06-08 01:39:58 -0700

    task 1 8

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include "main.h"
 
 /**
  * print_alphabet - Prints lowercase alphabets
- * for loop
- * Return: Always 0
  */
 
 void print_alphabet(void)
@@ -14,9 +9,8 @@ void print_alphabet(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		_putchar(ch);
-
+	}
 	_putchar('\n');
-
-	return (0);
 }
