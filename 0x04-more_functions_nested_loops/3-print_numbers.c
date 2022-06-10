7a27051d9cc5efc7d979880d3e74d9a7fb175690
bereket <emailbereket@gmail.com>
2022-06-10 06:43:06 -0700

    task 3 new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h" 
+
+/**
+ * print_numbers - prints number from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int (i);
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar("%d", i);
+	}
+	_putchar("\n");
+
+	return (0);
+}	
