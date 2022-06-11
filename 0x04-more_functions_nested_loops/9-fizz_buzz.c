639012ba57d897fb89647440f1eed5640e8814fb
bereket <emailbereket@gmail.com>
2022-06-11 01:02:53 -0700

    task 9 final

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,10 +13,10 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n % 1) == 0 && (n % 5) == 0)
+		if ((n % 3) == 0 && (n % 5) == 0)
 			printf("FizzBuzz");
 
-		else if ((n % 1) == 0)
+		else if ((n % 3) == 0)
 			printf("Fizz");
 
 		else if ((n % 5) == 0)
