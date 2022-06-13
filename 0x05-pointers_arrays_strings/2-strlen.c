614bb1ea84879473ec159bbc697d54a80def505c
bereket <emailbereket@gmail.com>
2022-06-13 13:20:55 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	_putchar("%d", _strlen(s));
+
+	return (0);
+}
