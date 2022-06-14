3ee03841e9ee0a4e95d4fd01bc42fee654eb2424
bereket <emailbereket@gmail.com>
2022-06-14 11:17:02 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of two string
+ * @str: the string containing the character
+ */
+
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
