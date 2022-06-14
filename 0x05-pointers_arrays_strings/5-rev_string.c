61d6c3684bf16200cd6d9eef413616cac87b33e6
bereket <emailbereket@gmail.com>
2022-06-14 11:09:19 -0700

    task five

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,8 +7,15 @@
 
 void rev_string(char *s)
 {
-	while (*s)
+	int len = 0, index = 0;
+	char tem;
+
+	while (s[index++])
+		len++;
+	for (index = len - 1; index >= len / 2; index--)
 	{
-		rev_string(s);
+		tem = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tem;
 	}
 }
