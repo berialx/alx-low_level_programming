1304a0473869ab41029e73e222efe8cbb641724f
bereket <emailbereket@gmail.com>
2022-06-15 11:54:40 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings upto n
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to use from source
+ *
+ * Return: string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1, len2, i;
+
+	len1 = strlen(dest);
+	len2 = strlen(src);
+
+	for (i = 0; i <= len2; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
+
+	dest[len1 + i] = '\0';
+
+	return (dest);
+}
