be6ca344d23e36902d88e068e24c89301a71a140
bereket <emailbereket@gmail.com>
2022-06-17 04:18:15 -0700

    task 1 17

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strncat - concatenates two strings upto n
@@ -12,20 +11,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len1, len2;
+	int index = 0, dest_len = 0;
 
-	int i = 0;
+	while (dest[index++])
+		dest_len++;
 
-	len1 = strlen(dest);
-	len2 = strlen(src);
-
-	while (dest[i++])
-		len1++;
-
-	for (i = 0; src[i] && i < n; i++)
-	{
-		dest[len1++] = src[i];
-	}
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
