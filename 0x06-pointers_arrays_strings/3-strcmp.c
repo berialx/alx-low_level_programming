3106ea246cfd867d5f161131d289116836c4c54b
bereket <emailbereket@gmail.com>
2022-06-17 05:05:24 -0700

    task 3 17

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,24 +11,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
-
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++)
+
+	if (s1[i] < s2[i])
 	{
-		if (s1[i] != s2[i])
-		{
-			flag = 1;
-
-			break;
-		}
-		else
-			return (0);
-
-	for (flag == 1)
-		if (s1[i] < s2[i])
-			return (negative);
-		else
-			return (positive);
-
+		return (-1);
+	}
+	else if (s1[i] > s2[i])
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
