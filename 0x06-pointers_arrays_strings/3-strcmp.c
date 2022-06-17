4fa956e32d8571b20da721dd39a320a0793dcd01
bereket <emailbereket@gmail.com>
2022-06-17 10:50:21 -0700

    task 3 10

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 
 /**
@@ -14,12 +15,10 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] == s2[i] && s1[i] == '\0'; i++)
 
-	if (s1[i] != s2[i])
-	{
-		return (s1[i] - s2[i]);
-	}
-	else
-	{
-		return (s1[i] - s2[i]);
-	}
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+
+	return (0);
 }
