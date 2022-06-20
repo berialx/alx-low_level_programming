0c91b78a59d227aa9f46d4298b2919a5c2fb0a5a
bereket <emailbereket@gmail.com>
2022-06-20 12:53:58 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to be checked
+ * @accept: the part of the string that we want
+ *
+ * Return: the number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int pos = _strspn(s, accept);
+}
