34d426d2116f29d1cef724dd0b114caf0cbe0cb3
bereket <emailbereket@gmail.com>
2022-06-20 14:14:40 -0700

    task 5 edited

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,15 +11,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+	int s = 0;
+
+	while (needle[s] != '\0')
+		s++;
+
 	while (*haystack)
 	{
-		if (*haystack != *needle)
+		for (i = 0; needle[i]; i++)
+		{
+			if (haystack[i] != needle[i])
+				break;
+		}
+		if (i != s)
 			haystack++;
 		else
 			return (haystack);
 	}
-	if (*needle == '\0')
-		return (haystack);
-
 	return (NULL);
 }
