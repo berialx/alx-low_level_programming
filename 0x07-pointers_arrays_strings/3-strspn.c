d54463417a5ea4cc24cbe8262e245ce0eb3e3654
bereket <emailbereket@gmail.com>
2022-06-20 13:21:26 -0700

    task 3 edited

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,5 +10,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int pos = _strspn(s, accept);
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+			return (c);
+
+	}
+	return (c);
 }
