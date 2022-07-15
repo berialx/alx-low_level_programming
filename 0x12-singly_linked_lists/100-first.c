9590af8b263e9c4977542b4f4a8a0c4eeb0fd7e7
bereket <emailbereket@gmail.com>
2022-07-15 05:07:13 -0700

    task 5 6

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,7 @@
 
 void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,");
+	printf("You're beat! and yet, you must allow,\n");
+
 	printf("I bore my house upon my back!\n");
 }
