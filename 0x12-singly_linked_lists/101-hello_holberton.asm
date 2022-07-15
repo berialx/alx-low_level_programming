055916c05c790489cb7e6beb93cb9d5d5b2d4be0
bereket <emailbereket@gmail.com>
2022-07-15 05:27:21 -0700

    task six

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
-section .data
-	fmt db "%s, 10, 0
-	msg db "Hello world!", 0	; 10 is the ASCII code for a new line (LF)
+section .text
+global main
 
-	section .text
-	extern printf
-	global main
 
 main:
-	push	rbp
-	mov	rsi, msg
-	mov	rdi, fmt
-	mov	rax, 0
-	call	printf
-	pop	rbp
-	mov	rax, 0
-	ret
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .rodata
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
