0f4e8c009480c1d6b559c5c690fc6d473ffe2157
bereket <emailbereket@gmail.com>
2022-07-21 13:16:48 -0700

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,41 +1,49 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * main - check the code for Holberton School students.
+ * _check_and_free - free a listint_t list
+ * @head: head of the list
+ * @prev: prev node in the list
  *
- * Return: Always 0.
+ * Return: number of nodes
  */
-int main(void)
+int _check_and_free(listint_t *head, listint_safe *prev)
 {
-    listint_t *head;
-    listint_t *head2;
-    listint_t *node;
+	listint_safe node, *tmp;
+	int count;
 
-    head2 = NULL;
-    add_nodeint(&head2, 0);
-    add_nodeint(&head2, 1);
-    add_nodeint(&head2, 2);
-    add_nodeint(&head2, 3);
-    add_nodeint(&head2, 4);
-    add_nodeint(&head2, 98);
-    add_nodeint(&head2, 402);
-    add_nodeint(&head2, 1024);
-    print_listint_safe(head2);
-    head = NULL;
-    node = add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    node->next = add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
-    print_listint_safe(head);
-    free_listint_safe(&head2);
-    free_listint_safe(&head);
-    printf("%p, %p\n", (void *)head2, (void *)head);
-    return (0);
+	if (head->next == NULL)
+	{
+		free(head);
+		return (1);
+	}
+
+	node.next = prev;
+	node.addy = head;
+	tmp = node.next;
+	while (tmp != NULL && tmp->addy != head)
+		tmp = tmp->next;
+	if (tmp != NULL)
+		return (0);
+
+	count = 1;
+	count += _check_and_free(head->next, &node);
+	free(head);
+	return (count);
+}
+/**
+ * free_listint_safe - free a listint_t safely
+ * @h: head of list
+ *
+ * Return: size of list free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	int total;
+
+	if (*h == NULL)
+		return (0);
+
+	total = _check_and_free(*h, NULL);
+	*h = NULL;
+	return (total);
 }
