5417fd1cacacb3ddb43be4c592a85e7801252bc6
bereket <emailbereket@gmail.com>
2022-12-14 02:01:37 -0800

    task bonus

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subraction
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulo
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
